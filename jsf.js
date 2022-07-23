5e693d845f865528c5786363cff418523ff4ebb8
gene-i9-ubuntu <gene.korn@bestme.co.il>
2022-07-23 22:38:29 +0300

    convert to class, sasha's tasks

--- jsf.js
+++ jsf.js
@@ -1,59 +1,80 @@
-const zero = '+[]';
-const one = '+!+[]';
+export default class Jsf {
+  constructor() {
+    this.zero = '+[]';
+    this.one = '+!+[]';
+    this.false = '![]';
+    this.true = '!![]';
+    this.falseStr = '![]+[]';
+    this.trueStr = '!![]+[]';
+    this.undefined = `[][${this.zero}]+[]`;
+    this.init();
+  }
 
-export function number(n) {
-    if (n === 0) return zero;
-    if (n === 1) return one;
-    return Array.from({ length: n - 1 }).reduce((res) => res + one, one);
-}
+  init() {
+    this.a = `(${this.falseStr})[${this.getNumber(1)}]`;
+    this.e = `(${this.trueStr})[${this.getNumber(3)}]`;
+    this.f = `(${this.falseStr})[${this.getNumber(0)}]`;
+    this.i = `(${this.undefined})[${this.getNumber(5)}]`;
+    this.l = `(${this.falseStr})[${this.getNumber(2)}]`;
+    this.n = `(${this.undefined})[${this.getNumber(1)}]`;
+    this.r = `(${this.trueStr})[${this.getNumber(1)}]`;
+    this.s = `(${this.falseStr})[${this.getNumber(3)}]`;
+    this.u = `(${this.trueStr})[${this.getNumber(2)}]`;
+    this.t = `(${this.trueStr})[${this.getNumber(0)}]`;
+    this['0'] = `(${this.getNumber(0)})+[]`;
+    this['1'] = `(${this.getNumber(1)})+[]`;
+    this['2'] = `(${this.getNumber(2)})+[]`;
+    this['3'] = `(${this.getNumber(3)})+[]`;
+    this['4'] = `(${this.getNumber(4)})+[]`;
+    this['5'] = `(${this.getNumber(5)})+[]`;
+    this['6'] = `(${this.getNumber(6)})+[]`;
+    this['7'] = `(${this.getNumber(7)})+[]`;
+    this['8'] = `(${this.getNumber(8)})+[]`;
+    this['9'] = `(${this.getNumber(9)})+[]`;
+    this.d = `(${this.undefined})[${this.getNumber(2)}]`;
+    /**  
+     *   3 = c, 6 = o, 8 = ' ', 15 = (, 16 = ), 18 = {,  20 = [,  32 = ],  34 = },
+     *  'a'['constructor']+[] => 'function filter() { [native code] }' */
+    this.c = `([][${this.getString('filter')}]+[])[${this.getNumber(3)}]`;
+    this.o = `([][${this.getString('filter')}]+[])[${this.getNumber(6)}]`;
+    this[' '] = `([][${this.getString('filter')}]+[])[${this.getNumber(8)}]`;
+    this['('] = `([][${this.getString('filter')}]+[])[${this['1']}+[${this.getNumber(5)}]]`;
+    this[')'] = `([][${this.getString('filter')}]+[])[${this['1']}+[${this.getNumber(6)}]]`;
+    this['{'] = `([][${this.getString('filter')}]+[])[${this['1']}+[${this.getNumber(8)}]]`;
+    this['['] = `([][${this.getString('filter')}]+[])[${this['2']}+[${this.getNumber(0)}]]`;
+    this[']'] = `([][${this.getString('filter')}]+[])[${this['3']}+[${this.getNumber(2)}]]`;
+    this['}'] = `([][${this.getString('filter')}]+[])[${this['3']}+[${this.getNumber(4)}]]`;
 
-//
+    /** 
+     *  9 = S, 14 = g
+     *  'a'['constructor']+[] => 'function String() { [native code] }' */
+    this.S = `(${this.a}[${this.getString('constructor')}]+[])[${this.getNumber(9)}]`;
+    this.g = `(${this.a}[${this.getString('constructor')}]+[])[${this['1']}+[${this.getNumber(4)}]]`;
+    this.filterStr = this.getString('filter')
+    this.toStringStr = this.getString('toString')
+  }
 
-export const map = {
-    a: `(![]+[]+[])[${number(1)}]`,
-    e: `(!+[]+[])[${number(3)}]`,
-    f: `(!!+[]+[])[${number(0)}]`,
-    i: `([][+![]]+[])[${number(5)}]`,
-    l: `(!!+[]+[])[${number(2)}]`,
-    n: `([][+![]]+[])[${number(1)}]`,
-    r: `(!+[]+[])[${number(1)}]`,
-    s: `(!!+[]+[])[${number(3)}]`,
-    u: `(!+[]+[])[${number(2)}]`,
-    t: `(!+[]+[])[${number(0)}]`,
-};
+  getNumber(n) {
+    if (n === 0) return this.zero;
+    if (n === 1) return this.one;
+    return Array.from({ length: n - 1 }).reduce((res) => res + this.one, this.one);
+  }
+  
+  getString(s) {
+      return s.split('').map((c) => {
+          return this[c];
+      }).join('+');
+  }
 
-map['0'] = `(${number(0)})+[]`;
-map['1'] = `(${number(1)})+[]`;
-map['2'] = `(${number(2)})+[]`;
-map['3'] = `(${number(3)})+[]`;
-map['4'] = `(${number(4)})+[]`;
-map['5'] = `(${number(5)})+[]`;
-map['6'] = `(${number(6)})+[]`;
-map['7'] = `(${number(7)})+[]`;
-map['8'] = `(${number(8)})+[]`;
-map['9'] = `(${number(9)})+[]`;
-
-map.c = `([][${string('filter')}]+[])[${number(3)}]`;
-map.d = `([][+![]]+[])[${number(2)}]`;
-map.o = `([][${string('filter')}]+[])[${number(6)}]`;
-map.S = `(${map.a}[${string('constructor')}]+[])[${number(9)}]`;
-map.g = `(${map.a}[${string('constructor')}]+[])[${map['1']}+[${number(4)}]]`;
-
-
-function string(s) {
-    return s.split('').map((c) => {
-        return map[c];
-    }).join('+');
+  execute(s) {
+    return `(()=>{})[${this.getString('constructor')}](${this.getString(s)})()`;
+  }
 }
 
 
 // map['.'] = `(+(${string('11e100')})+[])[${number(1)}]`;
 // map['-'] = `(+(${string('.0000001')})+[])[${number(2)}]`;
 // map.g = `(((/${map['-']}/)[${string('constructor')}])+[])[${number(11)}]`;
-
-function funcer(s, p) {
-    return `(()=>{})[${string('constructor')}](${string(s)})()`; // (${string(s)})('\\')
-}
 
 // console.log(number(0), '\n', eval(number(0)));
 // console.log(map['-'], '\n', eval(map['-']));
@@ -64,7 +85,3 @@ function funcer(s, p) {
 // console.log(eval(`(${map.a})[${string('constructor')}][${string('fromCharCode')}](${number(189)})`));
 
 // console.log(number(2));
-
-
-
-export default string
--- test/test.js
+++ test/test.js
@@ -1,75 +1,86 @@
-import string, { map, number } from '../jsf.js'
-import {expect, should, assert} from 'chai'
+import Jsf from '../jsf.js';
+import { expect, should, assert } from 'chai';
 import { describe } from 'mocha';
 
-const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
+const jsf = new Jsf();
 describe('acquire all number 0-9', () => {
-    describe('output "0" and "1"', () => {
-        it('(+![]) should output 0 (number)', () => {
-            expect(eval('+![]')).equals(0);
-        });
-    
-        it('(+!+[]) should output 1 (number)', () => {
-            expect(eval('+!+[]')).equals(1);
-        });
+  describe('output "0" and "1"', () => {
+    it('(jsf.zero) should output 0 (number)', () => {
+      expect(eval(jsf.zero)).equals(0);
     });
-    it('one + one should equal two', () => {
-        expect(eval('+!+[]+!+[]')).equals(2);
+
+    it('(jsf.one) should output 1 (number)', () => {
+      expect(eval(jsf.one)).equals(1);
     });
-    describe('the number function should return all the numbers 0-9', () => {
-        numbers.forEach((n) => {
-            it(`number(${n}) should return ${n} (number)`, () => {
-                expect(eval(number(n))).equals(n)
-            })
-        })
-    })
-    describe('all numbers should be converted to strings buy chaining +[]', () => {
-        numbers.forEach((n) => {
-            it(`number(${n})+[] should return "${n}" (string)`, () => {
-                expect(eval(`${number(n)}+[]`)).equals(n.toString())
-            })
-        })
-    })    
-})
+  });
+  it('one + one (concatenated) should equal two', () => {
+    expect(eval(jsf.one + jsf.one)).equals(2);
+  });
+  describe('the number function should return all the numbers 0-9', () => {
+    numbers.forEach((n) => {
+      it(`jsf.getNumber(${n}) should return ${n} (number)`, () => {
+        expect(eval(jsf.getNumber(n))).equals(n);
+      });
+    });
+  });
+  describe('all numbers should be converted to strings buy chaining +[]', () => {
+    numbers.forEach((n) => {
+      it(`jsf.getNumber(${n})+[] should return "${n}" (string)`, () => {
+        expect(eval(`${jsf.getNumber(n)}+[]`)).equals(n.toString());
+      });
+    });
+  });
+});
 
 describe('acquire the "constructor" letters ("c", "o", "n", "s", "t", "r", "u")', () => {
- it('(![]+[]) should output false (string)', () => {
-    expect(eval('![]+[]')).equals('false');
- });
- it('(!![]+[]) should output true (string)', () => {
-    expect(eval('!![]+[]')).equals('true');
- });
- it('([][+![]]) should output undefined (string) - now, we only need "c"', () => {
-    expect(eval('[][+![]]+[]')).equals('undefined')
- });
- it('string("filter") should output "filter" (string)', () => {
-    expect(eval(string('filter'))).equals('filter')
- });
+  it('(jsf.falseStr) should output false (string)', () => {
+    expect(eval(jsf.falseStr)).equals('false');
+  });
+  it('jsf.trueStr should output true (string)', () => {
+    expect(eval(jsf.trueStr)).equals('true');
+  });
+  it('jsf.undefined should output undefined (string) - now, we only need "c"', () => {
+    expect(eval(jsf.undefined)).equals('undefined');
+  });
+  it('jsf.getString("filter") should output "filter" (string)', () => {
+    expect(eval(jsf.getString('filter'))).equals('filter');
+  });
 
- it('string("c") should output "c" (string) ("([][${string(\'filter\')}]+[])[${number(3)}]" == "c")', () => {
-    expect(eval(string('c')), 'output' + string('c')).equals('c')
- })
- it('string("o") should output "o" (string) ("([][${string(\'filter\')}]+[])[${number(6)}]" == "o")', () => {
-    expect(eval(string('o')), 'output' + string('o')).equals('o')
- })
- it('string("constructor") should output "constructor" (string)', () => {
-    expect(eval(string('constructor'))).equals('constructor')
- });
+  it('jsf.getString("c") should output "c" (string) ("([][${jsf.getString(\'filter\')}]+[])[${jsf.getNumber(3)}]" == "c")', () => {
+    expect(eval(jsf.getString('c')), 'output' + jsf.getString('c')).equals('c');
+  });
+  it('jsf.getString("o") should output "o" (string) ("([][${jsf.getString(\'filter\')}]+[])[${jsf.getNumber(6)}]" == "o")', () => {
+    expect(eval(jsf.getString('o')), 'output' + jsf.getString('o')).equals('o');
+  });
+  it('jsf.getString("constructor") should output "constructor" (string)', () => {
+    expect(eval(jsf.getString('constructor'))).equals('constructor');
+  });
+  it('jsf.getString should print the rest of the acquired characters', () => {
+    expect(eval(jsf.getString('('))).equals('(');
+    expect(eval(jsf.getString(')'))).equals(')');
+    expect(eval(jsf.getString('{'))).equals('{');
+    expect(eval(jsf.getString('['))).equals('[');
+    expect(eval(jsf.getString(']'))).equals(']');
+    expect(eval(jsf.getString('}'))).equals('}');
+  });
+
 });
 
 describe('acquire the toString ability ie. the "S" and the "g" ', () => {
-    it(`"a"[\'constructor\']+[] should output a string that starts with "${String.toString()}"...`, () => {
-        expect(eval(`${map.a}[${string('constructor')}]+[]`)).equals(String.toString())
-    })
-    it('string("S") should output "S" (string) ("(${map.a}[${string(\'constructor\')}]+[])[${number(6)}]" == "S")', () => {
-        expect(eval(string('S'))).equals('S')
-    })
-    it('string("g") should output "g" (string) ("(${map.a}[${string(\'constructor\')}]+[])[${map[\'1\']}+[number(4)]]" == "g")', () => {
-        expect(eval(string('g'))).equals('g')
-    })
-    it('string("toString") should output "toString" (string)', () => {
-        expect(eval(string('toString'))).equals('toString')
-     });
+  it(`"a"[\'constructor\']+[] should output a string that starts with "${String.toString()}"...`, () => {
+    expect(eval(`${jsf.a}[${jsf.getString('constructor')}]+[]`)).equals(
+      String.toString()
+    );
+  });
+  it('jsf.getString("S") should output "S" (string) ("(${map.a}[${jsf.getString(\'constructor\')}]+[])[${jsf.getNumber(6)}]" == "S")', () => {
+    expect(eval(jsf.getString('S'))).equals('S');
+  });
+  it('jsf.getString("g") should output "g" (string) ("(${map.a}[${jsf.getString(\'constructor\')}]+[])[${map[\'1\']}+[jsf.getNumber(4)]]" == "g")', () => {
+    expect(eval(jsf.getString('g'))).equals('g');
+  });
+  it('jsf.getString("toString") should output "toString" (string)', () => {
+    expect(eval(jsf.getString('toString'))).equals('toString');
+  });
 });
-
